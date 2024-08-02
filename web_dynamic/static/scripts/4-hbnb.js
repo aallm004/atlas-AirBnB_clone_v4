68cf7dfe5a43f5552faac85ec65e0815a43621b0
aallm004 <abigailrosea10@gmail.com>
2024-08-02 18:32:01 -0500

    task 5

---
+++ web_dynamic/static/scripts/4-hbnb.js
@@ -0,0 +1,37 @@
+$(document).ready(function () {
+  $.get('http://0.0.0.0:5001/api/v1/status/', function (data) {
+      if (data.status === 'OK') {
+          $('#api_status').addClass('available');
+      } else {
+          $('#api_status').removeClass('available');
+      }
+  });
+
+  function fetchPlaces(amenities = {}) {
+  $.ajax({
+      url: 'http://0.0.0.0:5001/api/v1/places_search/',
+      type: 'POST',
+      contentType: 'application/json',
+      data: JSON.stringify(amenities),
+      success: function (data) {
+          for (const place of data) {
+              $('section.places').append(
+                  `<article>
+                      <div class="title_box">
+                          <h2>${place.name}</h2>
+                          <div class="price_by_night">$${place.price_by_night}</div>
+                      </div>
+                      <div class="information">
+                          <div class="max_guest">${place.max_guest} Guest${place.max_guest !== 1 ? 's' : ''}</div>
+                          <div class="number_rooms">${place.number_rooms} Bedroom${place.number_rooms !== 1 ? 's' : ''}</div>
+                          <div class="number_bathrooms">${place.number_bathrooms} Bathroom${place.number_bathrooms !== 1 ? 's' : ''}</div>
+                      </div>
+                      <div class="description">
+                          ${place.description}
+                      </div>
+                  </article>`
+              );
+          }
+        }
+    });
+  }});
